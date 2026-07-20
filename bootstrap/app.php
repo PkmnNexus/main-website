1a9db8e48c26b96557b3f44b392c75f06effb5d1
Michiel Elshout <michielelshout@iMac-van-Michiel.local>
2026-07-20 17:03:57 +0200

    Added Sentry for error reporting.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,7 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+        ->withExceptions(function (Exceptions $exceptions): void {
+        Integration::handles($exceptions);
+
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
         );
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Frontend\HomeController;
 
+Route::get('/sentry-test', function () {
+    throw new Exception('PkmnNexus manual browser test');
+});
+
 Route::get('/under-development', function () { return view('frontend.under-development'); })->name('under-construction');
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
