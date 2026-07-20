26c142517dbdf0e258325912b592ee5648d4abc8
Michiel Elshout <michielelshout@iMac-van-Michiel.local>
2026-07-20 18:24:19 +0200

    Added Michiel IP Adress to whitelist.

--- config/website.php
+++ config/website.php
@@ -4,7 +4,12 @@ return [
     'under_construction' => env('UNDER_CONSTRUCTION', true),
 
     'whitelist' => [
+        // Localhost
         '127.0.0.1',
         '::1',
+
+        // Michiel Home Torhout
+        ' 141.135.250.160',
+        '2001:4860:7:1506::f4'
     ],
 ];
